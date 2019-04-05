fcfdeedac04b1d03a438d5d7a1ff3639224cb34e
siporqueno <strike1000001@gmail.com>
2019-04-06 00:10:47 +0300

    Home work of Lesson 3, task 1.

---
+++ src/Lesson_3/LessonThreeHomeWorkTaskOne.java
@@ -0,0 +1,48 @@
+package Lesson_3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class LessonThreeHomeWorkTaskOne {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        playGame();
+        scanner.close();
+    }
+
+    private static void playGame() {
+        int responseOfPlayer = 2;
+        do {
+            playGameOnce();
+            do {
+                System.out.println("Повторить игру еще раз? 1-да/0-нет");
+                responseOfPlayer = scanner.nextInt();
+            } while (responseOfPlayer != 0 && responseOfPlayer != 1);
+        } while (responseOfPlayer == 1);
+    }
+
+    private static void playGameOnce() {
+        Random random = new Random();
+        int numberToGuess = random.nextInt(10);
+        System.out.println("Отгадайте с трех попыток целое число, которое не меньше 0 и не больше 9");
+        int flag = 0;
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Введите число: ");
+            int guess = scanner.nextInt();
+            if (guess==numberToGuess) {
+                System.out.println("Вы угадали!!!");
+                flag = 1;
+                break;
+            } if (guess > numberToGuess) {
+                System.out.println("Загаданное число меньше");
+            } else {
+                System.out.println("Загаданное число больше");
+            }
+        }
+        if (flag== 0) System.out.println("К сожалению, удача оказалась не на вашей стороне(");
+    }
+
+}
