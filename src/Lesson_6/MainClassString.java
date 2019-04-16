eee3325beebb96603e26037b62091fcee57988c1
siporqueno <strike1000001@gmail.com>
2019-04-16 18:00:00 +0300

    Home work of lesson 6. Task about Strings.

---
+++ src/Lesson_6/MainClassString.java
@@ -0,0 +1,32 @@
+package Lesson_6;
+
+public class MainClassString {
+    public static void main(String[] args) {
+
+        String str1 = " Предложение один    Теперь предложение два     Предложение три";
+        System.out.println(str1);
+
+        String str2 = str1.replaceAll(" +", " ").trim();
+        System.out.println(str2);
+
+        StringBuilder stringBuilder = new StringBuilder(str2);
+
+        int counter = 0;
+        for (int i = 1; i < str2.length(); i++) {
+            if (str2.charAt(i) >= 'A' && str2.charAt(i) <= 'Я') {
+//                stringBuilder.setCharAt(i - 1, '.');
+//                stringBuilder.insert(i, ' ');
+                stringBuilder.insert(i + counter - 1, '.');
+                counter++;
+            }
+        }
+
+        stringBuilder.insert(stringBuilder.length(), '.');
+
+        System.out.println(stringBuilder.toString());
+
+    }
+}
+
+
+// задание привести строку к нормальному ввиду
